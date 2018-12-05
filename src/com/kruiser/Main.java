415aa7e721c1f5958ece3cf18d7c8217905603b0
kruiser <battlekruiser@gmail.com>
2018-12-05 18:56:22 +0300

    map building done; changed number generation algorithm to avoid key collisions

--- src/com/kruiser/Main.java
+++ src/com/kruiser/Main.java
@@ -1,32 +1,62 @@
 package com.kruiser;
 
-import java.io.File;
+import javafx.util.Pair;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
 import static com.kruiser.FileUtils.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int a = (int)(Math.random()*100+1);
-        int b = (int)(Math.random()*100+1);
+        //init
+        int a = (int) (Math.random() * 100 + 1);
+        int b = (int) (Math.random() * 100 + 1);
         int t = a > b ? a : b;
-        int t2= a + b - t;
+        int t2 = a + b - t;
         List<String> raw, input;
+
+        //input
         try {
             raw = readAll("words.txt");
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("ну щито поделать");
             e.printStackTrace();
             return;
             //System.out.println("А хотя можно ещё раз попробовать");
         }
+
+        //randomness
         input = new ArrayList<String>();
-        for(String s : raw)
+        for (String s : raw) {
             input.addAll(Arrays.asList(s.split(";")));
-        for(String s : input)
-            System.out.println(s);
+        }
+        Collections.shuffle(input);
+        List<Integer> numbers = new ArrayList<>();
+        /*for (int i = 0; i < b; i++) {
+            numbers.add((int) (Math.random() * 1024)); //- так могут повторяться ключи
+        }*/
+        for(int i = 0; i < b; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers); //вот так хорошооо
+
+        //map
+        Map result;
+        if (a > b) {
+            result = new HashMap<String, Integer>();
+            for(int i = 0; i < a; i++) {
+                result.put(input.get(i), i <= b ? numbers.get(i) : null);
+            }
+        } else {
+            result = new HashMap<Integer, String>();
+            for(int i = 0; i < b; i++) {
+                result.put(numbers.get(i), i <= a ? numbers.get(i) : null);
+            }
+        }
+
+        //делаем что-нибудь с результатом
+        //туду//туду// тудудуду
     }
 }
